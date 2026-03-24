61b2831ae0f82f778c5c976ec2d17dd257b73f5a
Kanathip04 <67010974003@msu.ac.th>
2026-03-24 13:06:39 +0700

    0

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $heroImage .= '?v=' . time();
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>สถาบัน55555วิจัยวลัยรุกขเวช</title>
+<title>สถาบันวิจัยวลัยรุกขเวช</title>
 
 <style>
 *{

5e037e7a4b98c4ee655dd0750448f8169d13c18e
Kanathip04 <67010974003@msu.ac.thssh>
2026-03-27 00:51:02 +0700

    ฟ

--- register.php
+++ register.php
@@ -3,15 +3,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-session_start();
-require_once 'config.php';
-session_start();
-
 if (isset($_SESSION['user_id'])) {
     header("Location: dashboard.php");
     exit;
 }
-
+session_start();
 require_once 'config.php';
 
 $error_message = '';

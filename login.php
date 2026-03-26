7616184b8a5e45979ab4f9a687bb28db1bf0f1be
Kanathip04 <67010974003@msu.ac.th>
2026-03-26 17:33:33 +0700

    0

--- login.php
+++ login.php
@@ -2,12 +2,12 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 session_start();
-
+/*
 if (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'] === true) {
     header("Location: admin_dashboard.php");
     exit;
 }
-
+*/
 $message = $_SESSION['login_message'] ?? '';
 unset($_SESSION['login_message']);
 ?>

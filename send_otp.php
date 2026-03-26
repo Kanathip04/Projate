3593a247f1166b2b9deb3592f4f464f1a640fc03
Kanathip04 <67010974003@msu.ac.thssh>
2026-03-27 01:17:09 +0700

    ฟ

--- send_otp.php
+++ send_otp.php
@@ -1,4 +1,24 @@
 <?php
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+date_default_timezone_set('Asia/Bangkok');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    die('กรุณาเข้าหน้านี้ผ่านฟอร์ม login เท่านั้น');
+}
+
+require_once 'config.php';
+
+require __DIR__ . '/vendor/phpmailer/phpmailer/src/Exception.php';
+require __DIR__ . '/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require __DIR__ . '/vendor/phpmailer/phpmailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);

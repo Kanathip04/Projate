d0dc4438caf4ca72552d9ece1a5dfd418cf748ac
Kanathip04 <67010974003@msu.ac.thssh>
2026-03-27 01:35:11 +0700

    a

--- test.php
+++ test.php
@@ -1,4 +1,39 @@
 <?php
-require 'vendor/autoload.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-echo "PHPMailer พร้อมใช้งานแล้ว";
+require __DIR__ . '/vendor/phpmailer/phpmailer/src/Exception.php';
+require __DIR__ . '/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require __DIR__ . '/vendor/phpmailer/phpmailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = '67010974003@msu.ac.th';
+    $mail->Password   = 'uzotcwasteiaaroz';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
+
+    $mail->SMTPDebug  = 2;
+    $mail->Debugoutput = 'html';
+
+    $mail->CharSet = 'UTF-8';
+    $mail->setFrom('67010974003@msu.ac.th', 'Mail Test');
+    $mail->addAddress('67010974003@msu.ac.th');
+
+    $mail->isHTML(true);
+    $mail->Subject = 'PHPMailer Test';
+    $mail->Body    = 'ส่งเมลทดสอบสำเร็จ';
+
+    $mail->send();
+    echo 'ส่งเมลสำเร็จ';
+} catch (Exception $e) {
+    echo 'ส่งเมลไม่สำเร็จ: ' . $mail->ErrorInfo;
+}

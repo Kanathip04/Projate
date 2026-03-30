22e3603d5b8ff02835ab767306cb0b62e14c9f42
Kanathip04 <67010974003@msu.ac.th>
2026-03-31 01:18:25 +0700

    ฟ

--- payment_slip.php
+++ payment_slip.php
@@ -155,7 +155,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['slip'])) {
     $stmt2->close();
 }
 
-define('PROMPTPAY_ID', '06111360322');
+define('PROMPTPAY_ID', '0611360322');
 
 function promptpayPayload(string $target, float $amount): string
 {
